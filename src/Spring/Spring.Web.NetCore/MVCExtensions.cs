72f04810eb82c00bd76e3f40500d9c7501958683
黄剑 <MCTech11@LAPTOP-3CLB116V>
2021-08-09 16:28:55 +0800

    添加Spring.Web.NetCore

---
+++ src/Spring/Spring.Web.NetCore/MVCExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Spring.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Spring.Web.NetCore
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MVCExtensions
+    {
+        /// <summary>
+        ///  Use the Spring framework to load the controller
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="springContext"></param>
+        /// <returns></returns>
+        public static IMvcBuilder UseSpringControllerActivator(this IMvcBuilder builder, IApplicationContext springContext)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+            if(springContext == null)
+            {
+                throw new ArgumentNullException("springContext");
+            }
+            var activator = new SpringControllerActivator(springContext);
+            builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, SpringControllerActivator>((inter) => {
+                return activator;
+            }));
+            return builder;
+        }
+
+        /// <summary>
+        /// Use the Spring framework to load the controller
+        /// Before use this method, you need to make sure that Spring's IApplicationContext has been injected into builder.Services
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <exception cref="NullReferenceException">Fires when IApplicationContext does not exist builder.Services or is not a singleton</exception>
+        /// <returns></returns>
+        public static IMvcBuilder UseSpringControllerActivator(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+            Type springContextClazz = typeof(IApplicationContext);
+            if (builder.Services.Any(o => o.ServiceType.Equals(springContextClazz) && o.Lifetime == ServiceLifetime.Singleton))
+            {
+                builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, SpringControllerActivator>());
+            }
+            else
+            {
+                throw new NullReferenceException("Spring Context is not exist in builder.Services or is not Singleton");
+            }
+            return builder;
+        }
+    }
+}
---
+++ src/Spring/Spring.Web.NetCore/SpringControllerActivator.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Spring.Context;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace Spring.Web.NetCore
+{
+    /// <summary>
+    /// Use the Spring container to create the Controller object. If the specified object is not configured in the container, it will be created by default <see cref="DefaultControllerActivator"/>.
+    /// 
+    /// <see cref="IControllerActivator"/> that uses type activation to create controllers.
+    /// </summary> 
+    public class SpringControllerActivator : IControllerActivator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IApplicationContext context;
+        /// <summary>
+        /// 
+        /// </summary>
+        private static readonly TypeActivatorCache typeActivatorCache = new TypeActivatorCache();
+        /// <summary>
+        /// Creates a new <see cref="SpringControllerActivator"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="IApplicationContext"/>.</param>
+        public SpringControllerActivator(IApplicationContext context)
+        {
+            this.context = context;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="controllerContext"></param>
+        /// <returns></returns>
+        public object Create(ControllerContext controllerContext)
+        {
+            if (controllerContext == null)
+            {
+                throw new ArgumentNullException("actionContext");
+            }
+            var controllerTypeInfo = controllerContext.ActionDescriptor.ControllerTypeInfo;
+            if (controllerTypeInfo == null)
+            {
+                throw new ArgumentException("controllerTypeInfo is null");
+            }
+
+            Type serviceType = controllerTypeInfo.AsType();
+
+            var matchMap = context.GetObjectsOfType(serviceType);
+            if (matchMap.Any())
+            {
+                return matchMap.Values.First();
+            }
+            var serviceProvider = controllerContext.HttpContext.RequestServices;
+            return typeActivatorCache.CreateInstance<object>(serviceProvider, serviceType);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="controller"></param>
+        public void Release(ControllerContext context, object controller)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (controller == null)
+            {
+                throw new ArgumentNullException(nameof(controller));
+            }
+
+            if (controller is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+        /// <summary>
+        /// <see cref="Microsoft.AspNetCore.Mvc.Infrastructure.TypeActivatorCache"/>
+        /// </summary>
+        internal class TypeActivatorCache
+        {
+            private readonly Func<Type, ObjectFactory> _createFactory = (type) => ActivatorUtilities.CreateFactory(type, Type.EmptyTypes);
+            private readonly ConcurrentDictionary<Type, ObjectFactory> _typeActivatorCache = new ConcurrentDictionary<Type, ObjectFactory>();
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <typeparam name="TInstance"></typeparam>
+            /// <param name="serviceProvider"></param>
+            /// <param name="implementationType"></param>
+            /// <returns></returns>
+            public TInstance CreateInstance<TInstance>(IServiceProvider serviceProvider, Type implementationType)
+            {
+                if (serviceProvider == null)
+                {
+                    throw new ArgumentNullException(nameof(serviceProvider));
+                }
+
+                if (implementationType == null)
+                {
+                    throw new ArgumentNullException(nameof(implementationType));
+                }
+
+                var createFactory = _typeActivatorCache.GetOrAdd(implementationType, _createFactory);
+                return (TInstance)createFactory(serviceProvider, arguments: null);
+            }
+        }
+    }
+}
